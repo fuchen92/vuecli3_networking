cffe83f049422e3c51a187f35c1d30b6ba5b0012
fuchen92 <757213882@qq.com>
2019-07-30 14:41:21 +0800

    结构调整

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -13,7 +13,7 @@
             {{ $t('tabBar.plaza') }}
         </router-link>
         <router-link class="tabBarItemLink" to="/message" active-class="active">
-            <b class="tabBarRedDot" v-show="this.$route.path.substr(1) != 'message' && showRedDot"></b>
+            <b class="tabBarRedDot" v-show="this.$route.path != '/message' && showRedDot"></b>
             <span class="tabBarItemIcon tabBarMsg"></span>
             {{ $t('tabBar.message') }}
         </router-link>
